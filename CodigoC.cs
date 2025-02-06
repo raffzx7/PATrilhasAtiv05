00e5452a55fac584a171e335c218c59c6f8360f4
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-02-06 20:19:17 -0300

    Programa que calcule a idade de uma pessoa a partir de sua data de nascimento.

---
+++ CodigoC.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace fire
+{
+	class Program
+	{
+		public static void Main()
+		{
+			
+			Console.WriteLine("Insira a sua data de nascimento (dd/mm/yyyy): ");
+			DateTime dataaniversario = DateTime.Parse(Console.ReadLine());
+			
+			DateTime dataatual = DateTime.Now;
+			
+			TimeSpan datafinal = dataatual - dataaniversario;
+			
+			double anos = datafinal.TotalDays / 365.25;
+			double totaldias = Math.Abs(anos);
+
+			Console.WriteLine("A sua idade é " + totaldias.ToString("F0") + ".");
+			Console.ReadKey(true);
+		}
+    
+	}
+  
+}
