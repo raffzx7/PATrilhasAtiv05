1ea85f65fa24ffde10e37eb5ea0f6e292054333d
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-02-06 20:49:08 -0300

    Programa que solicite ao usuário para inserir um número inteiro positivo e, em seguida, exiba todos os números pares entre 0 e o número inserido.

---
+++ CodigosA.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("insira um número: ");
+			int numero = Convert.ToInt32(Console.ReadLine());
+			int contador = 0;
+			
+			Console.WriteLine("Os números pares entre 0 e {0} são: " ,numero);
+			
+			while (contador < numero) 
+			{
+				contador++;
+				
+				if (contador % 2 == 0 && contador != numero){
+				Console.WriteLine(contador);
+				}
+		}
+			Console.ReadKey(true);
+		}
+    
+	}
+  
+}
