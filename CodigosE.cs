9e77032a8594c2ac41ce75e2aa9113bcf44e33eb
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2025-02-06 21:05:55 -0300

    Programa que solicite ao usuário para inserir números inteiros até que um número negativo seja inserido. Em seguida, exiba a soma dos números inseridos (excluindo o número negativo).

---
+++ CodigosE.cs
@@ -0,0 +1,36 @@
+using System;
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main()
+		{	
+			int soma = 0;
+			string sequencia;
+			
+			Console.WriteLine("insira uma sequência de números inteiros(para encerrar a sequência digete algum núemro negativo): ");
+		
+			while (true)
+			{
+				sequencia = Console.ReadLine();
+				int numero;
+				
+				if (int.TryParse(sequencia, out numero) )
+				{
+					if (numero < 0) {
+						break;
+				    }
+					soma += numero;
+				}
+				
+				else {
+				    	Console.WriteLine("Insira somente valores inteiros.");
+				    }
+				}
+			Console.WriteLine("A soma dos números é {0}", soma);
+			Console.ReadKey(true);
+		}
+    
+	}
+  
+}
